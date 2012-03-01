380adb942a4522a4b8b3ac8f3188740b30e990dc
Mohammad Anwari <mohammad.anwari@nokia.com>
2012-03-01 17:01:14 +0200

    Changes: Removed jquerymobile as it leaks memory heavily

--- system/jqm-settings.js
+++
@@ -1,6 +0,0 @@
-$(document).bind("mobileinit", function(){
-    $.mobile.transitionFallbacks.slide = "slide";
-    $.mobile.page.prototype.options.addBackBtn   = true;
-    $.mobile.touchOverflowEnabled = true;
-
-});
