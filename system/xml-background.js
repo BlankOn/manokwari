2e19167af3790cb4b8c8c021eafccf0a10a708ea
Dhi Aurrahman <diorahman@gmail.com>
2014-02-06 13:00:41 +0700

    Changes: xml background states and transitions

--- system/xml-background.js
+++ system/xml-background.js
@@ -4,8 +4,8 @@ var _test = "  \
 <year>2011</year> \
 <month>11</month> \
 <day>24</day> \
-<hour>09</hour> \
-<minute>03</minute> \
+<hour>12</hour> \
+<minute>54</minute> \
 <second>00</second> \
 </starttime> \
  \
@@ -14,40 +14,40 @@ var _test = "  \
 <!-- We start with sunrise at 7 AM. It will remain up for 1 hour. --> \
 <static> \
 <duration>10.0</duration> \
-<file>/usr/share/themes/Adwaita/backgrounds/morning.jpg</file> \
+<file>/Users/diorahman/Experiments/projects/blankon/virtual/temp/themes/Adwaita/backgrounds/morning.jpg</file> \
 </static> \
  \
 <!-- Sunrise starts to transition to day at 8 AM. The transition lasts for 5 hours, ending at 1 PM. --> \
 <transition type='overlay'> \
 <duration>20.0</duration> \
-<from>/usr/share/themes/Adwaita/backgrounds/morning.jpg</from> \
-<to>/usr/share/themes/Adwaita/backgrounds/bright-day.jpg</to> \
+<from>/Users/diorahman/Experiments/projects/blankon/virtual/temp/themes/Adwaita/backgrounds/morning.jpg</from> \
+<to>/Users/diorahman/Experiments/projects/blankon/virtual/temp/themes/Adwaita/backgrounds/bright-day.jpg</to> \
 </transition> \
  \
 <!-- It's 1 PM, we're showing the day image in full force now, for 5 hours ending at 6 PM. --> \
 <static> \
 <duration>10.0</duration> \
-<file>/usr/share/themes/Adwaita/backgrounds/bright-day.jpg</file> \
+<file>/Users/diorahman/Experiments/projects/blankon/virtual/temp/themes/Adwaita/backgrounds/bright-day.jpg</file> \
 </static> \
  \
 <!-- It's 7 PM and it's going to start to get darker. This will transition for 6 hours up until midnight. --> \
 <transition type='overlay'> \
 <duration>20.0</duration> \
-<from>/usr/share/themes/Adwaita/backgrounds/bright-day.jpg</from> \
-<to>/usr/share/themes/Adwaita/backgrounds/good-night.jpg</to> \
+<from>/Users/diorahman/Experiments/projects/blankon/virtual/temp/themes/Adwaita/backgrounds/bright-day.jpg</from> \
+<to>/Users/diorahman/Experiments/projects/blankon/virtual/temp/themes/Adwaita/backgrounds/good-night.jpg</to> \
 </transition> \
  \
 <!-- It's midnight. It'll stay dark for 5 hours up until 5 AM. --> \
 <static> \
 <duration>15.0</duration> \
-<file>/usr/share/themes/Adwaita/backgrounds/good-night.jpg</file> \
+<file>/Users/diorahman/Experiments/projects/blankon/virtual/temp/themes/Adwaita/backgrounds/good-night.jpg</file> \
 </static> \
  \
 <!-- It's 5 AM. We'll start transitioning to sunrise for 2 hours up until 7 AM. --> \
 <transition type='overlay'> \
 <duration>7.0</duration> \
-<from>/usr/share/themes/Adwaita/backgrounds/good-night.jpg</from> \
-<to>/usr/share/themes/Adwaita/backgrounds/morning.jpg</to> \
+<from>/Users/diorahman/Experiments/projects/blankon/virtual/temp/themes/Adwaita/backgrounds/good-night.jpg</from> \
+<to>/Users/diorahman/Experiments/projects/blankon/virtual/temp/themes/Adwaita/backgrounds/morning.jpg</to> \
 </transition> \
 </background> \
 "; 
@@ -81,64 +81,99 @@ XmlBackground.prototype.load = function() {
 };
 
 XmlBackground.prototype.getItem = function() {
+
+  // get current date
+  var now = new Date();
   var acc = 0;
   var i = this.$data.children().length - 1;
-  var now = new Date();
-  var delta = (now - this.startTime)/1000;
-  console.log(this.startTime);
-  var lastDelta = 0;
-  var self = this;
-  while (i > 0) {
-    var item = this.$data.children()[i];
-    if (item.nodeName != "starttime" && item.delta <= delta) {
-      $("#cc").text(item.nodeName + " " + i);
-      if (item.nodeName == "transition" && item.delta > 0) {
-        if (self.percentageTimer) {
-          clearInterval(self.percentageTimer);
-        }
-        item.count = item.duration;
-        self.percentageTimer = setInterval(function() {
-          item.count --;
-          $("#cc").text(item.nodeName + delta + " " + i + " " + item.count);
-          $("#overlay").css("opacity", (item.percentage/item.duration));
-          if (item.count <= 0) {
-            clearInterval(self.percentageTimer);
-          }
-        }, 1000);
-      } else {
-        if (self.percentageTimer) {
-          clearInterval(self.percentageTimer);
-        }
-        item.count = item.duration;
-        self.percentageTimer = setInterval(function() {
-          item.count --;
-          $("#cc").text(item.nodeName + delta + " " + i + " " + item.count);
-          if (item.count <= 0) {
-            clearInterval(self.percentageTimer);
-          }
-        }, 1000);
+  var delta = (now - this.startTime) / 1000;
 
-      }
-      var when;
-      if (lastDelta > 0) {
-        when = (delta - item.delta) * 1000;
+  var self = this;
+  
+  while (i > 0) {
+    
+    var item = this.$data.children()[i];
+
+    if (item.nodeName != "starttime" && delta >= item.delta ) {
+      
+      // handle item
+      self.handleItem (item);
+
+      var hasNext = i < (this.$data.children().length - 1);
+      var nextItem;
+      var when = 0;
+
+      if (hasNext) {
+        // get next item
+        nextItem = this.$data.children()[ i + 1];
+        when = Math.ceil(nextItem.delta - delta);
+      } 
+
+      // set handler
+      if (nextItem && when) {
+
+        self.debugCount = when;
+
+        if (self.timeout) {
+          clearTimeout(self.timeout);  
+        }
+        
+        // get next state, self.timeout is handler for getting next item
+        self.timeout = setTimeout(function(){
+          self.getItem();
+        }, when * 1000);
+
+        if (item.nodeName == "transition") {
+          
+          // when
+          self.transitionDuration = when;
+          self.transitionCounter = when;
+
+          if (self.transition) {
+            clearInterval(self.transition);
+          }
+
+          self.transition = setInterval (function (){
+            
+            // percentage 
+            var percentage = (self.transitionCounter + 0.0) / self.transitionDuration;
+
+            // set opacity
+            $("#bg").css("opacity", 1 - percentage);
+            $("#overlay").css("opacity", percentage);
+
+            self.transitionCounter--;
+
+          }, 1000);
+        }
       } else {
-        when = (delta - item.delta) * 1000;
+        self.debugCount = 0;
       }
-      if (this.timeout) {
-        clearTimeout(this.timeout);
+
+      if (self.counter) {
+        clearInterval(self.counter);  
       }
-      console.log("w:%d", when);
-      this.timeout = setTimeout(function() {
-        var s = XmlBackground.prototype.instance;
-        s.getItem();
-      }, when);
-      this.handleItem(item);
-      lastDelta = item.delta;
+      
+      // Open following lines to debug
+      // start counter for debugging
+      / * 
+      if (self.debugCount) {
+        self.counter = setInterval(
+        function(){
+          self.debugCount--;
+          $("#debug").text(item.nodeName + " - " + i + " count:" + self.debugCount + " seconds");
+        }, 1000);
+      } else {
+
+        $("#debug").text("");
+        clearInterval(self.counter);  
+
+      }*/
+      
+
       break;
-    }
-    lastDelta = item.delta;
-    i --;
+    } 
+    i--;
   }
 }
 
@@ -146,15 +181,18 @@ XmlBackground.prototype.handleItem = function(item) {
   var file;
   if (item.nodeName == "static") {
     file = item.querySelector("file").textContent;
-    $("body").css("background-image", "url(" + file + ")");
+    $("#bg").css("background-image", "url(" + file + ")");
   } else {
     overlay = item.querySelector("from").textContent;
     file = item.querySelector("to").textContent;
-    $("#overlay").css("background-image", "url(" + file + ")");
-    $("body").css("background-image", "url(" + overlay + ")");
+
+    $("#bg").css("opacity", 0.0);
+    $("#bg").css("background-image", "url(" + file + ")");
+
+    $("#overlay").css("opacity", 1.0);
+    $("#overlay").css("background-image", "url(" + overlay + ")");
+
   }
-  console.log(item.i);
-  console.log(file);
 }
 
 XmlBackground.prototype.run = function() {
@@ -177,14 +215,12 @@ XmlBackground.prototype.run = function() {
     }
   }
 
-  var item = this.getItem();
-  console.log(this.$data.children());
+  this.getItem();
 };
 
 $(document).ready(function() {
   var x = new XmlBackground();
   var p = $($.parseXML(_test));
-  console.log(p);
   x.$data = p.children();
   x.run();
 });
