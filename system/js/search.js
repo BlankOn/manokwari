649d0b5a4dde3122516a42dff473182a07839fbd
Herpiko Dwi Aguno <herpiko@gmail.com>
2016-12-13 23:40:00 +0700

    Run commands directly from search form. Forward search to Google or Wikipedia entry. Credit @winardiaris

--- system/js/search.js
+++ system/js/search.js
@@ -1,28 +1,29 @@
-(function($) {
-	jQuery.expr[':'].Contains = function(a,i,m){
+(function ($) {
+  jQuery.expr[':'].Contains = function (a, i, m) {
     return (a.textContent || a.innerText || "").toUpperCase().indexOf(m[3].toUpperCase()) >= 0;
   };
-	// First condition
-	function firstCond(){
-		$('input').val("");
-		$('#listSearch .ui-listview-item').hide();
+  // First condition
+  function firstCond() {
+    $('input').val("");
+    $('#listSearch .ui-listview-item').hide();
 	};
-	function unselect(){
+
+	function unselect() {
 		$('#listSearch').find('.selected').removeClass('selected');
 		$('#listSearch').find('.active').removeClass('active');
 	}
 	// Accending sort
-	function asc_sort(a, b){
+	function asc_sort(a, b) {
 		return ($(b).text()) < ($(a).text()) ? 1 : -1;    
 	}
 	// Decending sort
-	function dec_sort(a, b){
+	function dec_sort(a, b) {
 		return ($(b).text()) > ($(a).text()) ? 1 : -1;    
 	}
-	// remove duplicate
-	function removeDup(a){
+	// Remove duplicate
+	function removeDup(a) {
 		var seen = {};
-		$(a).each(function() {
+		$(a).each(function () {
 			var txt = $(this).text();
 				if (seen[txt]) {
 					$(this).remove();
@@ -31,19 +32,35 @@
         }
 		});
 	}
+
+  function isValidUrl(url) {
+    var urlRegex = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/
+    return urlRegex.test(url);
+  }
+
+  function visitLink(a) {
+    Utils.run_command('x-www-browser ' + a);
+  }
+
+  function runTerminal(a) {
+    Utils.run_command('gnome-terminal -e "' + a + '"');
+  }
+
 	var typingTimeout;
 	function listFilter(header, list) { 
 	  // Header is any element, list is an unordered list
     // Create and add the filter form to the header
-    var form = $("<div>").attr({"class":"filterform"}),
-        input = $("<input>").attr({"class":"filterinput","type":"text","id":"searchinput","placeholder":"Type to search..."});
+    var form = $("<div>").attr({"class":"filterform"});
+    var input = $("<input>").attr({"class":"filterinput","type":"text","id":"searchinput","placeholder":"Type to search..."});
+    var runin = $('<div>').attr({'class':'runin'});
     $(form).append(input).appendTo(header);
+    $(form).append(runin).appendTo(header);
    
 	  // Arrow navigation
    
 
     $(input)
-		.click( function(){firstCond();unselect();})
+		.click( function () {firstCond();unselect();})
 		.keyup( function (e) {
 		  var $item = $('#listSearch a span:visible').not(':has(:empty)');
 			var o = {38: 'up',40: 'bottom',37: 'prev',39: 'next'}
@@ -51,10 +68,32 @@
 		  var $active = $('.active'),
 		  i = $item.index($active);
 			
-			if (e.which == 13) {
+			if (e.which === 13) {
+				var filter = $(this).val();
+        if (filter.indexOf('!r') > -1 && filter.length === 2) {
+          return runTerminal('pkill manokwari');
+				}
+
 		    $('span.active').parent().addClass('selected');
-		    var a = $active.parent().attr('desktop')
-				Utils.run_desktop(a);
+        var p = $active.parent();
+				if (p.hasClass('runin-item-text')) {
+					var r = p.attr('runin');
+					if (p.parent().hasClass('url')) {
+						visitLink(r);
+					} else if (p.parent().hasClass('search')) {
+						var a = "https://www.google.com/search?q="+r;
+						visitLink(a);
+					} else if (p.parent().hasClass('wiki')) {
+						var a = "https://en.wikipedia.org/wiki/"+r;
+						visitLink(a);
+					} else {
+						runTerminal(r);
+					}
+				} else {
+          var a = $active.parent().attr('desktop');
+					Utils.run_desktop(a);
+				}
+
 				firstCond();
 				unselect();
 		  } else if (e.which === 38 || e.which === 40 ) {
@@ -77,7 +116,7 @@
 				$(list).find(".ui-listview-item").hide();
 				if (filter && filter != " " && filter != "" && filter.length > 0) {
 				  clearTimeout(typingTimeout);
-					typingTimeout = setTimeout(function() {
+					typingTimeout = setTimeout(function () {
 						$(list).hide();				
 	
 						// The query executed immediatelly for each keypress, this will cause performance issue
@@ -93,7 +132,18 @@
 										
 						$(list).find('a span:visible').first().addClass('active');
 						$(list).find(".active").parent().parent().addClass('selected');
-						
+					
+            // runin
+            $('.runin-item').remove();
+            if (isValidUrl(filter)) {
+              var runinUrl = $("<div class='ui-listview-item runin-item url'><a href='#' class='runin-item-text' runin='"+filter+"'><img src='img/logo_globe.png'><span>Open URL: "+filter+"</span></a>");
+              $(list).append(runinUrl);
+            } else {
+							var runinCommand = $("<div class='ui-listview-item runin-item command'><a href='#' class='runin-item-text' runin='"+filter+"'><img src='img/logo_command.png'><span>Run: "+filter+"</span></a>");
+              var runinSearch = $("<div class='ui-listview-item runin-item search'><a href='#' class='runin-item-text' runin='"+filter+"'><img src='img/logo_google.png'><span>Search: "+filter+"</span></a>");
+              var runinWiki = $("<div class='ui-listview-item runin-item wiki'><a href='#' class='runin-item-text' runin='"+filter+"'><img src='img/logo_wikipedia.png'><span>Wikipedia: "+filter+"</span></a>");
+              $(list).append(runinCommand).append(runinSearch).append(runinWiki);
+            }
 					}, 500);
 				} else {
 					$(list).find(".ui-listview-item").hide();
@@ -108,18 +158,18 @@
 		});
   }
   // On DOM ready
-	$(document).ready(function() {
+	$(document).ready(function () {
 		var header = $('#header'),
 				listSearch = $('#listSearch'),
 				listSearch_child = $('#listSearch .ui-listview-item');	
-		$(document).keydown(function() {$("input").focus();});
+		$(document).keydown(function () {$("input").focus();});
 		listFilter(header, listSearch);
 		listSearch_child.sort(asc_sort).appendTo(listSearch);
-		listSearch_child.mouseup(function() {
+		listSearch_child.mouseup(function () {
       firstCond();
     });
 		removeDup(listSearch_child);
-		$("#listApplications").click(function() { 
+		$("#listApplications").click(function () { 
       firstCond();
     });
 	});
