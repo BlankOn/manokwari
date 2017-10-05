25f0ac182a345f33f3094aec50a5f3f2b6de8c72
Sucipto <show.cheap@gmail.com>
2017-10-05 14:17:23 +0700

    Update compile run script inside Docker container

--- scripts/compile-run.sh
+++ scripts/compile-run.sh
@@ -2,5 +2,5 @@
 
 # Supposed to run inside a Docker container
 adduser --disabled-password --gecos "" test
-(cd /manokwari && make && make install)
-sudo -H -u test /usr/bin/manokwari
+(cd /manokwari && ./autogen.sh && make && make install)
+sudo -H -u test XDG_SESSION_TYPE=x11 /usr/bin/blankon-session
